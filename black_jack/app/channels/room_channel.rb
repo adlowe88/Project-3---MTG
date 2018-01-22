32cd7caf8135e10a62653fd45125b422cb72e541
Damien Lowe <ashleydamienlowe@gmail.com>
2018-01-22 16:42:23 +1100

    room_channel

--- black_jack/app/channels/room_channel.rb
+++ black_jack/app/channels/room_channel.rb
@@ -8,7 +8,26 @@ class RoomChannel < ApplicationCable::Channel
     client_action = data[ "message" ]
     @game = Game.find_by( started: false ) || Game.find( @current_user.game.id )
     @current_user.user_action( client_action[ "user_action" ] ) if client_action[ "user_action" ]
+
+    if client_action["join"]
+      player = @current_user
+      @game.add_player( player)
+    elsif client_action["start-game"]
+      start_game( @game )
+    elsif @game.started
+      @game = Game.find( @game.id )
+      game_play( @game )
+    end
   end
+
+  private
+
+    def game_play( game )
+      
+    end
+end
+
+
 
 
 end
