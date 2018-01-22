169af8ed38efebdab875dcf9a618ad7573c1bbb2
JustinGan <jgan91@gmail.com>
2018-01-23 10:29:19 +1100

    a lot of cards now render to screen

--- black_jack/app/controllers/cards_controller.rb
+++ black_jack/app/controllers/cards_controller.rb
@@ -1,8 +1,10 @@
 class CardsController < ApplicationController
   def show
-    @card = Card.where( user_id: params[ :id ] )
+    @card = Card.where( user_id: params[ :user_id ] )
     respond_to do |format|
       format.html { redirect_to @card }
       format.js {}
       format.json { render json: @card, location: @card }
+    end
+  end
 end
